f236af52bb36579e5ccc09b66e663b4b51a5342a
wy <wy@wys-MacBook-Pro.local>
2018-04-03 22:13:55 +0800

    .

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,18 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  render(){
+    return(
+      <div>
+        <h1>React 小白</h1>
       </div>
-    );
+    )
   }
 }
 
