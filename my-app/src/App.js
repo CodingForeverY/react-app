b8cad2e8f73e67e2f16dfc84d66e3e37f731a910
wy <wy@wys-MacBook-Pro.local>
2018-04-11 10:26:39 +0800

    修改页面

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import CommentApp from './CommentApp'
-import './App.css';
+import './style/App.css';
 
 class App extends Component {
   render(){
